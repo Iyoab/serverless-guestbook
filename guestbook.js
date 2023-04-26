29de89aca54ed4134980125c1644f72de7ecf4cc
Iyoab <122353068+Iyoab@users.noreply.github.com>
2023-04-26 13:56:34 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/6f71f90a-e56c-4045-9206-d2cebeefa44a/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
